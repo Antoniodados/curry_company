b0d6bf8ebd8d0761ff59b81d342a35a163d75738
Antoniodados <113479129+Antoniodados@users.noreply.github.com>
2023-10-15 08:21:18 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,7 +19,7 @@ st.write( "# Curry Company Growth Dashboard" )
 
 st.markdown(
     """
-    Growth Dashboard foi construído para acmpanhar as métricas de crescimento dos Entregadores  Restaurantes.
+    Growth Dashboard foi construído para acompanhar as métricas de crescimento dos Entregadores  Restaurantes.
     ### Como utilizar esse Growth Dashboard?
     - Visão Empresa:
         - Visão Gerencial: Métricas gerais de comportamento.
